72c9ac66d31d6f467c89611d762248c6ce06551f
Tanujairam <tanujairam.v@gmail.com>
2025-04-27 19:23:51 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-// tailwind.config.js
-theme: {
-  extend: {
-    backdropBlur: {
-      xs: '2px',
-    },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
   },
-}
+  plugins: [],
+};
