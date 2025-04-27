df3517b0e0f01065543acad9ae1125739d1768b9
Tanujairam <tanujairam.v@gmail.com>
2025-04-27 21:11:03 +0530

    Create MainPage.js

---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+// src/components/MainPage.js
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Main Page!</h1>
+    </div>
+  );
+};
+
+export default MainPage;
