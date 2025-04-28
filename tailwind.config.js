d713169c034e3e75a51023228676090c7659e993
Tanujairam <tanujairam.v@gmail.com>
2025-04-28 23:08:52 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    'hover:bg-accent-magenta/10',
+    'dark:hover:bg-accent-cyan/10',
+  ],
   theme: {
     extend: {
       fontFamily: {
