761c86503c022f5332924fa4c2d4bcf37bcd4277
Tanujairam <tanujairam.v@gmail.com>
2025-04-30 17:46:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import React, { useState } from 'react';
-import { AnimatePresence } from 'framer-motion';
-import IntroScreen from './components/IntroScreen';
 import Portfolio from './components/Portfolio';
-import Navbar from './components/Navbar';
 
 function App() {
-  const [showIntro, setShowIntro] = useState(true);
-
-  const handleIntroComplete = () => {
-    setShowIntro(false);
-  };
-
-  return (
-    <>
-      {!showIntro && <Navbar />}
-      <AnimatePresence>
-        {showIntro ? (
-          <IntroScreen onComplete={handleIntroComplete} />
-        ) : (
-          <Portfolio />
-        )}
-      </AnimatePresence>
-    </>
-  );
+  return <Portfolio />;
 }
 
 export default App;
