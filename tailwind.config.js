edd29fc8cc1bac130b6ab09dd916ec30a3b947a3
Tanujairam <tanujairam.v@gmail.com>
2025-05-02 20:47:28 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neon-blue': '#00f0ff',
+        'neon-pink': '#ff00cc',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
