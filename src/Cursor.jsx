1f3b9bff16279f541351c8f05f111ab767eed1cc
Tanujairam <tanujairam.v@gmail.com>
2025-05-02 21:30:06 +0530

    Update Cursor.jsx

--- src/Cursor.jsx
+++ src/Cursor.jsx
@@ -18,7 +18,7 @@ function Cursor() {
     };
 
     const unhoverCursor = () => {
-      gsap.to(cursorRef.current, { scale: 1, opacity: 1 });
+      gsap.to(cursorRef.current, { scale: 1, opacity: 0.7 });
     };
 
     window.addEventListener('mousemove', moveCursor);
@@ -39,7 +39,7 @@ function Cursor() {
   return (
     <div
       ref={cursorRef}
-      className="fixed cursor-invert"
+      className="fixed cursor-glass"
       style={{ transform: 'translate(-50%, -50%)' }}
     />
   );
