47d1f919dc1f273475ba5e290944ce214c038b71
Tanujairam <tanujairam.v@gmail.com>
2025-05-02 21:44:25 +0530

    Update ProjectCard.jsx

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -7,17 +7,15 @@ function ProjectCard({ name, url, tech }) {
       whileHover={{ scale: 1.05 }}
       transition={{ duration: 0.3 }}
     >
-      <h3 className="text-xl font-poppins">{name}</h3>
-      <p className="mt-2 text-sm text-gray-400">
-        Technologies: {tech.join(', ')}
-      </p>
+      <h3 className="text-xl font-montserrat text-primary">{name}</h3>
+      <p className="mt-2 text-secondary font-inter">{tech.join(', ')}</p>
       <a
         href={url}
         className="mt-4 inline-block btn"
         target="_blank"
         rel="noopener noreferrer"
       >
-        View on GitHub
+        View Project
       </a>
     </motion.div>
   );
