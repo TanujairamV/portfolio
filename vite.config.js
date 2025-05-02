3e53a74ed7f0c472c7f94671d2939fab80ba0591
Tanujairam <tanujairam.v@gmail.com>
2025-05-02 17:55:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,12 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]',
+      },
+    },
   },
 });
