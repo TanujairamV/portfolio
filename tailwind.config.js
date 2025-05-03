2612a9ab87611429d532c306d02698debb310a71
Tanujairam <tanujairam.v@gmail.com>
2025-05-03 21:09:30 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        chillax: ['Chillax', 'sans-serif'],
-        'plus-jakarta': ['Plus Jakarta Sans', 'sans-serif'],
-        'general-sans': ['General Sans', 'sans-serif'],
-        satoshi: ['Satoshi', 'sans-serif'],
-        'clash-grotesk': ['Clash Grotesk', 'sans-serif'],
+        'cabinet-grotesk': ['"Cabinet Grotesk"', 'sans-serif'],
+        ranade: ['Ranade', 'sans-serif'],
+        'dancing-script': ['"Dancing Script"', 'cursive'],
+        inter: ['Inter', 'sans-serif'],
+        'space-grotesk': ['"Space Grotesk"', 'sans-serif'],
       },
     },
   },
