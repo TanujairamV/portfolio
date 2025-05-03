8c438953ceac49f39a2d6273124142e026dcbf68
Tanujairam <tanujairam.v@gmail.com>
2025-05-03 23:03:53 +0530

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,17 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        'cabinet-grotesk': ['Cabinet Grotesk', 'sans-serif'],
+        ranade: ['Ranade', 'sans-serif'],
+        'dancing-script': ['Dancing Script', 'cursive'],
+        'space-grotesk': ['Space Grotesk', 'sans-serif'],
+      },
+    },
   },
-}
+  plugins: [],
+};
