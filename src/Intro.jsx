5d5f007f88c7645062034986791d6585e3b85fcc
Tanujairam <tanujairam.v@gmail.com>
2025-05-04 00:16:35 +0530

    Update Intro.jsx

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -7,14 +7,14 @@ function Intro({ onComplete }) {
       className="fixed inset-0 flex items-center justify-center bg-dark z-50"
       initial={{ opacity: 1 }}
       animate={{ opacity: 0 }}
-      transition={{ duration: 1.2, delay: 1.5 }}
+      transition={{ duration: 1.5, delay: 2 }}
       onAnimationComplete={onComplete}
     >
       <motion.h1
-        className="text-5xl font-cabinet-grotesk text-heading"
-        initial={{ y: 30, opacity: 0 }}
+        className="text-6xl font-cabinet-grotesk text-heading"
+        initial={{ y: 40, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
+        transition={{ duration: 0.8 }}
       >
         Tanujairam
       </motion.h1>
