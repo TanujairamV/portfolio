aeb98e7d5fa72c6bffff0710c7d53f3ad502922a
Tanujairam <tanujairam.v@gmail.com>
2025-05-04 22:25:19 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        seafoam: {
+          100: '#E6F3F3',
+          200: '#CDE7E8',
+          500: '#A7D8D9',
+          600: '#8CC6C7',
+          700: '#70B4B5',
+          800: '#559FA0',
+        },
+      },
+      borderRadius: {
+        '2xl': '1rem',
+      },
+    },
   },
   plugins: [],
 };
