59e80fa48d5c507c29bbe2dd12bfa9f700867736
Tanujairam <tanujairam.v@gmail.com>
2025-05-04 21:48:50 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,19 +5,19 @@ function Footer() {
     <footer className="py-4 text-center font-inter text-subheading">
       <div className="flex justify-center space-x-2 mb-1">
         <a href="https://instagram.com/tanujairam.v" className="social-icon material-icon" target="_blank" rel="noopener noreferrer">
-          <FaInstagram size={16} />
+          <FaInstagram size={14} />
         </a>
         <a href="https://t.me/Tanujairam" className="social-icon material-icon" target="_blank" rel="noopener noreferrer">
-          <FaTelegram size={16} />
+          <FaTelegram size={14} />
         </a>
         <a href="https://discord.com/users/Tanujairam" className="social-icon material-icon" target="_blank" rel="noopener noreferrer">
-          <FaDiscord size={16} />
+          <FaDiscord size={14} />
         </a>
         <a href="mailto:tanujairam.v@gmail.com" className="social-icon material-icon" target="_blank" rel="noopener noreferrer">
-          <FaEnvelope size={16} />
+          <FaEnvelope size={14} />
         </a>
         <a href="https://github.com/TanujairamV" className="social-icon material-icon" target="_blank" rel="noopener noreferrer">
-          <FaGithub size={16} />
+          <FaGithub size={14} />
         </a>
       </div>
       <p className="text-xs">© 2025 Tanujairam</p>
