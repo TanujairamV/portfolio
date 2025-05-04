98c5971208c8aed976acebc630d44002eca79907
Tanujairam <tanujairam.v@gmail.com>
2025-05-04 22:06:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/portfolio/', // Replace with your repository name for GitHub Pages
 });
