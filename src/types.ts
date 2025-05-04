a1b0960747b6d74eef38a378580dd8db1b485595
Tanujairam <tanujairam.v@gmail.com>
2025-05-05 03:15:07 +0530

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface Project {
+  name: string;
+  url: string;
+  tech: string[];
+  description: string;
+}
+
+export interface Skill {
+  name: string;
+  proficiency: string;
+  icon: string;
+}
