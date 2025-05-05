aa8c68dfae389747a7eaa9837b29127417ece7eb
Tanujairam <tanujairam.v@gmail.com>
2025-05-05 22:58:31 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  darkMode: 'class',
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
+        'space-grotesk': ['Space Grotesk', 'sans-serif'],
         'cabinet-grotesk': ['Cabinet Grotesk', 'sans-serif'],
         ranade: ['Ranade', 'sans-serif'],
-        'dancing-script': ['Dancing Script', 'cursive'],
-        'space-grotesk': ['Space Grotesk', 'sans-serif'],
       },
       colors: {
-        dark: '#121212',
-        light: '#FFFFFF',
-        accent: '#6B46C1',
-        secondary: '#FF6AC1',
-        hover: '#FFD700',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        subheading: 'var(--subheading)',
+        heading: 'var(--heading)',
+        accent: 'var(--accent)',
+        secondary: 'var(--secondary)',
       },
     },
   },
