1268770f5ebc9a1a86da5145957722d322e6d23d
Tanujairam <tanujairam.v@gmail.com>
2025-05-05 22:57:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace with your repository name for GitHub Pages
+  base: '/portfolio/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
 });
