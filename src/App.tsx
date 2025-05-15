beb73c0e7276d331a7b57e5de6e68aedf9f228b7
Tanujairam <tanujairam.v@gmail.com>
2025-05-15 21:56:13 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Portfolio from './Portfolio';
 
-function App() {
-  return <Portfolio />;
-}
+const App = () => {
+  return (
+    <div>
+      <Portfolio />
+    </div>
+  );
+};
 
 export default App;
