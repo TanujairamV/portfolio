3a67c1806db5d6ef162f87692c6f03e1f90dc0fe
Tanujairam <tanujairam.v@gmail.com>
2025-05-18 23:34:35 +0530

    Update Index.tsx

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./main.css";
+import "./Styles.css";
 
 // Create root element and render App within React.StrictMode
 const rootElement = document.getElementById("root");
